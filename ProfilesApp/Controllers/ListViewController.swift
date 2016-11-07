f95eb2f98b074c2ae5983f8151a6561378224577
Dagna Bieda <dbieda@quoininc.com>
2016-11-07 16:32:36 -0500

    Added ListViewController with some default mehods, that need to be overwritten

---
+++ ProfilesApp/Controllers/ListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListViewController.swift
+//  ProfilesApp
+//
+//  Created by Dagna Bieda on 11/7/16.
+//  Copyright © 2016 Dagna Bieda. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ListViewController: UITableViewController {
+
+    // MARK: UIViewController Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: UITableViewDelegate methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+
+}
