d51ff00e7eb7672236a03c0742e1bdbbe2262393
Dagna Bieda <dagna.bieda@gmail.com>
2016-11-17 13:44:47 -0500

    Connecting ProfileVC's outlets

--- ProfilesApp/Controllers/ProfileViewController.swift
+++ ProfilesApp/Controllers/ProfileViewController.swift
@@ -10,4 +10,9 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var gender: UITextField!
+    @IBOutlet weak var hobbies: UITextField!
 }
