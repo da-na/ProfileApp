8fbdfce1cc385baed4f3a3637380618c1338ea72
Dagna Bieda <dagna.bieda@gmail.com>
2016-11-18 15:52:28 -0500

    Added functionality to ProfileViewController to scroll it whenever keyboard pops up

--- ProfilesApp/Controllers/ProfileViewController.swift
+++ ProfilesApp/Controllers/ProfileViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var backgroundScroll: UIScrollView!
+    @IBOutlet weak var containerStackView: UIStackView!
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var age: UITextField!
     @IBOutlet weak var gender: UITextField!
     @IBOutlet weak var hobbies: UITextField!
+
+    // MARK: VCLifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector:  #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector:  #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    // MARK: Keyboard handling methods
+    @objc private func keyboardWillShow(notification: NSNotification){
+        let infoKey: AnyObject  = notification.userInfo![UIKeyboardFrameEndUserInfoKey]! as AnyObject
+        let keyboardFrame = view.convert(infoKey.cgRectValue!, from: nil)
+
+        offsetBackgroundScrollViewToKeepTextFieldsVisible(keyboardFrame: keyboardFrame)
+    }
+    @objc private func keyboardWillHide(){
+        backgroundScroll.contentOffset = CGPoint(x: 0, y: 0)
+    }
+
+    // MARK: Helper methods
+    private func offsetBackgroundScrollViewToKeepTextFieldsVisible(keyboardFrame: CGRect){
+        let stackViewFrame = view.convert(containerStackView.bounds, from: containerStackView)
+        let margin: CGFloat = 12.0
+
+        let difference = (keyboardFrame.origin.y - margin) - (stackViewFrame.origin.y + stackViewFrame.height)
+        if  difference >= 0 {
+            // Keyboard doesn't obscure last visible text view, no need to scroll
+        } else {
+            backgroundScroll.contentOffset = CGPoint(x: 0, y: -difference)
+        }
+    }
 }
